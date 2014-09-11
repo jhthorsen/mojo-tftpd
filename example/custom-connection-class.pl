1ae4fae14a31ac8a598bfc4ee0ea6a406e395f8c
Jan Henning Thorsen <jhthorsen@cpan.org>
2014-09-11 12:16:57 +0200

    Try to make it a bit more robust (still failing)

--- example/custom-connection-class.pl
+++ example/custom-connection-class.pl
@@ -1,17 +1,20 @@
-use strict;
-use warnings;
-use Test::More;
+use Mojo::Base -strict;
 use Mojo::TFTPd;
 use Mojo::UserAgent;
-
-plan skip_all => 'HTTP_TEST=1' unless $ENV{HTTP_TEST};
 
 {
   package Mojo::TFTPd::Connection::HTTP;
   use Mojo::Base 'Mojo::TFTPd::Connection';
 
+  sub receive_ack {
+    my $self = shift;
+    $self->{can_push_data} = 1;
+    $self->SUPER::receive_ack(@_);
+  }
+
   sub send_data {
     my $self = shift;
+
     return 1 if exists $self->{http_buffer} and !length $self->{http_buffer};
     return $self->SUPER::send_data(@_);
   }
@@ -20,10 +23,9 @@ plan skip_all => 'HTTP_TEST=1' unless $ENV{HTTP_TEST};
 package main;
 my $tftpd = Mojo::TFTPd->new(listen => 'localhost:7000', connection_class => 'Mojo::TFTPd::Connection::HTTP');
 my $ua = Mojo::UserAgent->new;
-my $c;
 
 $tftpd->on(rrq => sub {
-    (my $tftpd, $c) = @_;
+    my ($tftpd, $c) = @_;
     my $file = $c->file;
 
     if ($file =~ m!^https?://!) {
@@ -31,18 +33,25 @@ $tftpd->on(rrq => sub {
       $tx->res->max_message_size(0);
 
       $c->{http_buffer} = '';
+      $c->{can_push_data} = 1;
       open my $FH, '<', \$c->{http_buffer} or die $!;
       $c->filehandle($FH);
 
       Scalar::Util::weaken($c);
+
+      # send the remaining chunk
+      $tx->on(finish => sub { $c and $c->send_data; });
+
+      # stream HTTP chunks
       $tx->res->content->unsubscribe('read')->on(read => sub {
         my ($content, $bytes) = @_;
+        return unless $c;
         $c->{http_buffer} .= $bytes;
-        $c->send_data;
+        $c->send_data if delete $c->{can_push_data} and length($c->{http_buffer}) % $c->blocksize;
       });
 
       # callback is called when all data is fetched
-      $ua->start($tx, sub { delete $c->{http_buffer}; });
+      $ua->start($tx, sub {});
     }
     else {
       # ...
@@ -51,5 +60,3 @@ $tftpd->on(rrq => sub {
 );
 
 $tftpd->start->ioloop->start;
-
-done_testing;
